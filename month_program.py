4f7cc85ac55ccc42b04565901e7d3efb9dfa106f
Ubuntu <ubuntu@ip-172-31-30-141.ec2.internal>
2019-10-07 21:08:10 +0000

    month program

---
+++ month_program.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+# Created by: Euel Yirga
+# Created on: Oct 2019
+# This program says if a number is positive or negative
+
+
+def main():
+    # input
+    number = int(input("Please enter a number: "))
+
+    # process
+    if number == 1:
+        # output
+        print()
+        print("January")
+
+    # process
+    elif number == 2:
+        # output
+        print()
+        print("February")
+
+    # process
+    elif number == 3: 
+        # output
+        print()
+        print("March")
+
+    #process
+    elif number == 4:
+        #output
+        print()
+        print("April")
+        
+    #process
+    elif number == 5:
+        #output
+        print()
+        print("May")
+        
+    #process
+    elif number == 6:
+        #output
+        print()
+        print("June")
+        
+    #process
+    elif number == 7:
+        #output
+        print()
+        print("July")
+        
+    #process
+    elif number == 8:
+        #output
+        print()
+        print("August")
+        
+    #process
+    elif number == 9:
+        #output
+        print()
+        print("September")
+    
+    #process    
+    elif number == 10:
+        #output
+        print()
+        print("October")
+        
+    #process
+    elif number == 11:
+        #output
+        print()
+        print("November")
+        
+    #process
+    elif number == 12:
+        #output
+        print()
+        print("December")
+
+if __name__ == "__main__":
+    main()
